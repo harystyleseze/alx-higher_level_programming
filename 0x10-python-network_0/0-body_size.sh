391272c18ea68163814191c32cad8f958bfa4c78
harystyles <harystyleseze@gmail.com>
2024-07-06 15:17:11 -0700

    quick fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# send a request to a URL with curl, and displays the size of the body of the response
-curl -s "$1" | wc -c
+# Sends a request to a URL and displays the size of the body of the response
+curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
 
